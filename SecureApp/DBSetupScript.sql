45a573ea9a7ee125a05ee2be3465f44247550113
Rohit Dhingra <Rohit Dhingra@DESKTOP-TQ58HNN>
2019-03-04 16:25:49 +0530

    BCrypt Password Encoder

--- SecureApp/DBSetupScript.sql
+++ SecureApp/DBSetupScript.sql
@@ -10,3 +10,9 @@ insert into user values(103,'Spiderman','web');
 insert into user values(104,'Superman','fly');
 
 select * from user;
+
+ALTER TABLE `jmd`.`user` 
+CHANGE COLUMN `password` `password` VARCHAR(200) NULL DEFAULT NULL ;
+
+insert into user values(101,'rohit','$2a$10$Ezd6s61bBDdTN4vl6nSAYObBGtIy0j1bzx9C8LbRiuTrv6TqxMEre');
+select * from user;
--- SecureApp/src/main/java/com/poc/secureapp/config/AppSecurityConfig.java
+++ SecureApp/src/main/java/com/poc/secureapp/config/AppSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +22,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	{
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		return provider;
 	}
 	
