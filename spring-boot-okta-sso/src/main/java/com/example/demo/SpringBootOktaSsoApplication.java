38e4f21ab177458e0376df1f2a1f099ab16b5cae
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-05-16 14:24:36 +0530

    OKta Project Integration

---
+++ spring-boot-okta-sso/src/main/java/com/example/demo/SpringBootOktaSsoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class SpringBootOktaSsoApplication {
+	
+	@GetMapping("/")
+	public String welcome2User(Principal principal)
+	{
+		return "Hi " +principal.getName() + "Welcome to Learning World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOktaSsoApplication.class, args);
+	}
+
+}
