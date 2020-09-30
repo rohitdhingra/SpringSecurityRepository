ac3b7c7e03d0e1c7bafebbd9b5d3d96f01fc07b1
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-10-01 01:44:54 +0530

    In Memory Spring Security Application

---
+++ InMemorySpringSecurityPOC/src/main/java/com/example/security/demo/SpringStartingPoint.java
@@ -0,0 +1,13 @@
+package com.example.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartingPoint {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartingPoint.class, args);
+	}
+
+}
---
+++ InMemorySpringSecurityPOC/src/main/java/com/example/security/demo/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.example.security.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World!!!";
+	}
+}
